cb4d04c64d562a54714032c073d711dabc7ede11
James E. Marca <james@activimetrics.com>
2016-10-05 12:06:49 -0700

    include tile in handle topo code
    have to make sure to do
    
    d3.tile = require('d3-tile').tile or the way webpack interacts with d3
    means that d3 doesn't have a tile function

--- src/static/handleTopo.js
+++ src/static/handleTopo.js
@@ -1,10 +1,34 @@
 var d3 = require('d3')
+d3.tile = require('d3-tile').tile
+
 var topojson = require('topojson')
 
-function zoomed(g,mesh){
+function stringify(scale, translate) {
+  var k = scale / 256, r = scale % 1 ? Number : Math.round;
+  return "translate(" + r(translate[0] * scale) + "," + r(translate[1] * scale) + ") scale(" + k + ")";
+}
+
+function zoomed(raster_g,vector_g,tile,mesh){
     return function(){
         var transform = d3.event.transform
-        g.attr("transform",transform)
+        vector_g.attr("transform",transform)
+        // var tiles = tile
+        //         .scale(transform.k)
+        //         .translate([transform.x, transform.y])
+        // ();
+
+        // var image = raster_g
+        //         .attr("transform", stringify(tiles.scale, tiles.translate))
+        //         .selectAll("image")
+        //         .data(tiles, function(d) { return d; });
+        // image.exit().remove();
+        // image.enter().append("image")
+        //     .attr("xlink:href", function(d) { return "http://" + "abc"[d[1] % 3] + ".tile.openstreetmap.org/" + d[2] + "/" + d[0] + "/" + d[1] + ".png"; })
+        //     .attr("x", function(d) { return d[0] * 256; })
+        //     .attr("y", function(d) { return d[1] * 256; })
+        //     .attr("width", 256)
+        //     .attr("height", 256);
+
         return null
     }
 }
